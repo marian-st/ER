f974ecc9c7cbb0b24326d94144f710cebb87b605
EliaPiccoli <eliakoby@live.it>
2019-07-11 17:51:09 +0200

    Table view added to HP interface, prepared it to inject data from state

--- src/main/java/InterfaceController/HPController.java
+++ src/main/java/InterfaceController/HPController.java
@@ -1,5 +1,6 @@
 package InterfaceController;
 
+import State.Entities.Patient;
 import State.StateEvent;
 import State.Store;
 import State.StringCommand;
@@ -7,20 +8,32 @@ import System.LoginDemo.HP.HPComponent;
 import System.LoginDemo.LoginComponent;
 import System.LoginDemo.Sistema;
 import io.reactivex.subjects.Subject;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
 
 import java.util.UUID;
 
 public class HPController {
     private Store<StringCommand> store;
     private final Sistema sys = Sistema.getInstance();
+    @FXML private TableView<Patient> table;
 
     public HPController(Store<StringCommand> store, Subject<StateEvent> stream) {
         this.store = store;
 
+        //TODO: sub2PewDiePie
+
+        //TODO: get from state patients and write them into the table
+        ObservableList<Patient> data  = table.getItems();
+        //data.addAll(); --> var args of all the patients from state
     }
 
     @FXML protected void showPatients() {
+        //TODO: get from state patients and write them into the table
+        ObservableList<Patient> data  = table.getItems();
+        //data.addAll(); --> var args of all the patients from state
+
         sys.setInterface("HPDF", HPComponent.HPTitle);
     }
 
