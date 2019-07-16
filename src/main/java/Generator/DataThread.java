f5099e765e7c077b3fb7e30ca89aed1e9ecebf51
EliaPiccoli <eliakoby@live.it>
2019-07-16 18:06:00 +0200

    DataGenerator rebuild to be one task that generates all the data

--- src/main/java/Generator/DataThread.java
+++ src/main/java/Generator/DataThread.java
@@ -1,23 +1,40 @@
 package Generator;
 
-public class DataThread extends Thread{
-    private final RandomGaussian random;
-    private final long timeToWait;
-    private final Value valueHandled;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-    public DataThread(double mean, double variance, long timeToWait, Value value) {
-        this.random = new RandomGaussian(mean, variance);
-        this.timeToWait = timeToWait;
-        this.valueHandled = value;
+public class DataThread extends Thread{
+    private final int numTask = 4;
+    private final Long[] times = {2000L, 2000L, 3000L, 5000L}; //2-2-3-5 (s)
+    private final Value[] taskJob = {Value.SBP, Value.DBP, Value.HEART_RATE, Value.TEMPERATURE};
+    private ArrayList<TimerTask> tasks = new ArrayList<>();
+    private ArrayList<Timer> timers = new ArrayList<>();
+
+    public DataThread() {
+        for(int i=0; i<numTask; i++) {
+            this.tasks.add(new LoopTask(taskJob[i]));
+            this.timers.add(new Timer());
+        }
     }
 
     public void start() {
-        try {
-            //Send message to state to update monitored value -> see valueHandled
-            System.out.println("Thread to generate " + valueHandled + " generated: " + random.getValue());
-            this.sleep(timeToWait);
-        } catch (Exception e) {
-            e.printStackTrace();
+        for(int i=0; i<numTask; i++)
+            this.timers.get(i).scheduleAtFixedRate(this.tasks.get(i), new Date(), times[i]);
+    }
+
+    /**
+     * this class will create the various Gaussian based on the Value passed to the constructor
+     */
+    private class LoopTask extends TimerTask {
+        private Value value;
+
+        public LoopTask(Value v) {
+            this.value = v;
+        }
+        public void run() {
+            System.out.println(this.value);
         }
     }
 }
