0a80e5d41ef2fe577cc58c7792348e1e6435ecfd
EliaPiccoli <eliakoby@live.it>
2019-07-19 01:25:54 +0200

    AlarmController now evolves one random patient thread

---
+++ src/main/java/InterfaceController/AlarmsController.java
@@ -0,0 +1,46 @@
+package InterfaceController;
+
+import Generator.Sickness;
+import State.StateEvent;
+import State.Store;
+import State.StringCommand;
+import System.Sistema;
+import io.reactivex.subjects.Subject;
+import javafx.fxml.FXML;
+
+public class AlarmsController {
+    private final Sistema sys = Sistema.getInstance();
+    private Store<StringCommand> store;
+
+    public AlarmsController(Store store, Subject<StateEvent> stream) {
+        this.store = store;
+    }
+
+    @FXML protected void aritmia(){
+        store.update(new StringCommand("EVOLVE_GENERATOR", Sickness.BRACHICARDIA));
+    }
+
+    @FXML protected void tachicardia(){
+        store.update(new StringCommand("EVOLVE_GENERATOR", Sickness.TACHICARDIA));
+    }
+
+    @FXML protected void ipotermia(){
+        store.update(new StringCommand("EVOLVE_GENERATOR", Sickness.IPOTERMIA));
+    }
+
+    @FXML protected void ipertermia(){
+        store.update(new StringCommand("EVOLVE_GENERATOR", Sickness.IPERTERMIA));
+    }
+
+    @FXML protected void ipotensione(){
+        store.update(new StringCommand("EVOLVE_GENERATOR", Sickness.IPOTENSIONE));
+    }
+
+    @FXML protected void ipertensione(){
+        store.update(new StringCommand("EVOLVE_GENERATOR", Sickness.IPERTENSIONE));
+    }
+
+    @FXML protected void flutter(){
+        store.update(new StringCommand("EVOLVE_GENERATOR", Sickness.FLUTTER));
+    }
+}
