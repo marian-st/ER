f0b997dcdb130b35276118c6cc3b68f67408cf77
EliaPiccoli <eliakoby@live.it>
2019-07-19 13:23:15 +0200

    AlarmNotLogged functioning and implemented

---
+++ src/main/java/InterfaceController/AlarmControlController.java
@@ -0,0 +1,39 @@
+package InterfaceController;
+
+import Component.HPComponent;
+import Entities.Role;
+import Entities.User;
+import State.StateEvent;
+import State.Store;
+import State.StringCommand;
+import System.Sistema;
+import io.reactivex.subjects.Subject;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AlarmControlController {
+    private final Sistema sys = Sistema.getInstance();
+    private Store<StringCommand> store;
+    @FXML private TextField userField;
+    @FXML private PasswordField passField;
+
+    public AlarmControlController(Store store, Subject<StateEvent> stream) {
+        this.store = store;
+        stream.subscribe(se -> {
+            if (se.command().name().equals("ALM_LOGIN_SUCCESS") || se.command().name().equals("ALM_LOGIN_FAILURE")) {
+                userField.clear();
+                passField.clear();
+                if (se.command().name().equals("ALM_LOGIN_SUCCESS")) {
+                    store.update(new StringCommand("RESET"));
+                } else {
+                    System.out.println("Invalid username and/or password");
+                }
+            }
+        });
+    }
+
+    @FXML protected void login() {
+        store.update(new StringCommand("ALARM_LOGIN", new User(userField.getText(), passField.getText())));
+    }
+}
