e8bb88bf7caf7a4de6a9105635129acfa01008ac
Edoardo Z <edovi@icloud.com>
2019-07-22 00:02:31 +0200

    Improves MutableItem

--- src/main/java/Main/MutableItem.java
+++ src/main/java/Main/MutableItem.java
@@ -8,6 +8,7 @@ import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 public class MutableItem<T> {
@@ -15,8 +16,8 @@ public class MutableItem<T> {
     private List<T> history = new ArrayList<>();
 
     private Subject<T> life$ = PublishSubject.create();
-    private int bufferSize = 10;
-    private int replay = 1;
+    private int bufferSize = 11;
+    private int cursor = 0;
 
     public MutableItem(T item) {
         this.item = item;
@@ -24,14 +25,33 @@ public class MutableItem<T> {
     }
 
     public MutableItem buffer(int s) {
-        if (s < 0 || s > 20) bufferSize = 10;
-        else bufferSize = s;
+        if (s < 0 || s > 20) bufferSize = 11;
+        else bufferSize = s+1;
 
         return this;
     }
 
     public Disposable subscribe(Consumer<T> function) {
         return this.life$.subscribe(function::accept);
+    }
+
+    public Disposable subscribeWithLast(BiConsumer<T, T> function) {
+        return this.life$.subscribe(t -> function.accept(t, history.get(1)));
+    }
+
+    public Disposable subscribeWithLast(int size, BiConsumer<T, List<Optional<T>>> function) {
+
+        return this.life$.subscribe(t -> {
+            List<Optional<T>> arr = new ArrayList<>();
+            for (int i = 1; i < size + 1 && i < bufferSize; i++) {
+                try {
+                    arr.add(Optional.of(history.get(i)));
+                } catch (IndexOutOfBoundsException err) {
+                    arr.add(Optional.empty());
+                }
+            }
+            function.accept(t, arr);
+        });
     }
 
     public static MutableItem of(Object item) {
@@ -41,10 +61,13 @@ public class MutableItem<T> {
     public void change(T item) {
         this.item = item;
         this.history.add(0, item);
+        if (cursor++ >= bufferSize) {
+            history.remove(cursor--);
+        }
         this.life$.onNext(item);
     }
 
-    public Optional<T> last () {
+    public Optional<T> last() {
         if (history.size() > 0) return Optional.of(history.get(0));
         else return Optional.empty();
     }
---
+++ src/main/java/Main/MutableTest.java
@@ -0,0 +1,21 @@
+package Main;
+
+import Entities.Patient;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public class MutableTest {
+    public static void main(String ...args) {
+        MutableItem<Integer> a = new MutableItem<Integer>(3);
+        a.subscribeWithLast(10, (p, l) ->
+                System.out.println(String.valueOf(p) + " | " + String.valueOf(l)));
+
+        for (int i = 1; i < 18; i ++ ) {
+            a.change(a.last().get()+1);
+        }
+
+    }
+}
